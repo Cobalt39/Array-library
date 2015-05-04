a6f88c5245da8ed4169ec5f807df3393ef9ae0aa
csimnitt8846 <csimnitt8846@001LAB102010549.neisd.ad>
2015-05-04 15:54:34 -0500

    zdj7

---
+++ Array-library/src/Books.java
@@ -0,0 +1,28 @@
+
+public class Books implements Items {
+
+	
+	
+	
+	
+	@Override
+	public void Turninorout() {
+		
+	}
+
+	@Override
+	public void Searchup() {
+		
+	}
+
+	@Override
+	public boolean availabilty() {
+		return false;
+	}
+
+	@Override
+	public String Name() {
+		return null;
+	}
+
+}
--- Array-library/src/Customer.java
+++ Array-library/src/Customer.java
@@ -7,43 +7,37 @@ public class Customer implements People {
 	
 	@Override
 	public void Customer() {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public long getID() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void setID(long id) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public String setFName(String fname) {
-		// TODO Auto-generated method stub
 		return fName = fname;
 	}
 
 	@Override
 	public String getFName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String setLName(String lname) {
-		// TODO Auto-generated method stub
-	return	lName = lname;
+			lName = lname;
 	}
 
 	@Override
 	public String getLName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
