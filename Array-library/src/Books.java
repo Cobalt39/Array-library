61d99e8eb0148dff55e0207c088af66f67f2628e
mburgos5617 <mburgos5617@001LAB102010535.neisd.ad>
2015-05-07 15:17:17 -0500

    JFrame

--- Array-library/src/Books.java
+++ Array-library/src/Books.java
@@ -1,4 +1,7 @@
-
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+ 
 public class Books implements Items {
 
 	
@@ -7,7 +10,18 @@ public class Books implements Items {
 	
 	@Override
 	public void Turninorout() {
-		
+		JFrame Turninorout = new JFrame();
+		Turninorout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 Turninorout.setVisible(true);
+	        Turninorout.setSize(1000, 800);
+	    JPanel one = new JPanel();
+	    JLabel lable = new JLabel("Are you turning in a book");
+	    
+	    
+	    Turninorout.add(one);
+	    one.add(lable);
+	    
+	       
 	}
 
 	@Override
